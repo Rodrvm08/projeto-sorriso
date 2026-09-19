6d5251b676faa929b85c7b94a49e6f9eda2961fd
Rodrigo Vilela Magalhães <rodrvm08@gmail.com>
2026-09-19 16:24:34 -0300

    commit final

--- src/components/historia/index.tsx
+++ src/components/historia/index.tsx
@@ -1,13 +1,14 @@
 import { HistoriaSection, TextContainer, Title, Paragraph, ImageContainer, Image } from "./style";
 import { useEffect, useState } from "react";
 
+import img0 from '../../assets/img0.jpg'
 import img1 from '../../assets/img1.jpg'
 import img2 from '../../assets/img2.jpg'
 import img3 from '../../assets/img3.jpg'
 import img4 from '../../assets/img4.jpg'
 
 
-const images = [img1, img2, img3, img4]
+const images = [img0, img1, img2, img3, img4]
 
 export default function Historia() {
     const [currentImage, setCurrentImage] = useState(0);
